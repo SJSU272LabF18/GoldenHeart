e8e279e863102a8eddfd945392e51f787d5b1ae2
Saket Thakare <saketthakare@gmail.com>
2018-12-16 00:08:46 -0800

    reorg
    Reorganizing repo for better readability

--- GoldenHeartProject/frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
-
-const rootReducer = combineReducers({
-  //reducer: mainreducer,
-  form: formReducer
-});
-
-export default rootReducer;
